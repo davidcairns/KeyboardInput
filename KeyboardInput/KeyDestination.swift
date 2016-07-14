30ec62e0dc8b2de02987dba3e1bb05eb15a09efc
David Cairns <dcairns@oblong.com>
2016-07-14 13:56:34 -0400

    Fixes out of order key events

--- KeyboardInput/KeyDestination.swift
+++ KeyboardInput/KeyDestination.swift
@@ -17,9 +17,13 @@ final class KeyDestination {
 
   func post(keyCode: UInt16, flags: CGEventFlags = CGEventFlags(rawValue: 0)!, isDown: Bool) {
     // Create an key event with this code (& flags) and post it to our app’s process.
-//    TODO: Create key events using CGSCreateKeyboardEventOfLength? (I have gotten warnings in the console about this…) --DRC
-    let keyEvent: CGEventRef = CGEventCreateKeyboardEvent(nil, CGKeyCode(keyCode), isDown)!
+    // TODO: Create key events using CGSCreateKeyboardEventOfLength? (I have gotten warnings in the console about this…) --DRC
+    let eventSource = CGEventSourceCreate(CGEventSourceStateID.CombinedSessionState)
+    let keyEvent: CGEventRef = CGEventCreateKeyboardEvent(eventSource, CGKeyCode(keyCode), isDown)!
     CGEventSetFlags(keyEvent, flags)
     CGEventPostToPSN(self.app.processSerialNumber.ptr(), keyEvent)
+
+    // Maybe this is a dumb way to solve this, but let's sleep for just a moment so they events don’t arrive out of order.
+    usleep(10)
   }
 }
